420c1ae7a66725c89b7b47d60fdc237c9aea4459
Ksenx73 <hello@itcampe.com>
2021-10-14 16:32:19 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,12 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 546;
+
+        int lastDigit; //напишите тут ваш код
+
+        System.out.println(lastDigit);
+    }
+}
